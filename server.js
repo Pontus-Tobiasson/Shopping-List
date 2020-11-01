6cc1b27d31d002021539913eaa620cb3ea2feeb4
P0ntus <Pontustobiasson@gmail.com>
2020-11-01 13:21:03 +0100

    changed dburl

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 //const dburl = "mongodb://localhost:27017/";
-const dburl = "mongodb://heroku_0hc88pq5:ajv5eoc8L2xCb@ds259577.mlab.com:59577";
+const dburl = "mongodb://heroku_0hc88pq5:ajv5eoc8L2xCb@ds259577.mlab.com:59577/heroku_0hc88pq5";
 const port = process.env.PORT || 8080;
-const database = "heroku_0hc88pq5"; // shoppingdb
+const database = ""; // shoppingdb
 
 
 
